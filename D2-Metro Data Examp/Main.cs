b41b2ccb1cff6aec39daf91619197b8a1194716f
Ranur-react <rahmatnur845@gmail.com>
2021-11-24 15:33:08 +0700

    inherithen

--- D2-Metro Data Examp/Main.cs
+++ D2-Metro Data Examp/Main.cs
@@ -20,6 +20,7 @@ public class Program
 public class MenuApp{
 		private static String[] menu={"Kategori","Data Barang","Penjualan","Exit"}; //saya igin membuat data ini dinamis ditambahkan disaatn Objek ini dipanggil taip tidak tau yang bagaiaman mengganti nilai array variabel  sebuah objek ketika pemanggilan objek , masih bingung
 		public String appTitle;
+		private int sizePaper=60;
 	public MenuApp(){
 		//constructor----
 		appTitle="Aplikasi Penjualan";
@@ -31,41 +32,72 @@ public class MenuApp{
 	}
 	public void displayMenu(){
 		bool state=true;
+		
+		Table tb=new Table();
+		
+		//Contoh inherithance untuk mengganti lebar Layout Table
+		tb.Width(60);
+			//tb.setBodyElement("x");
 		int menulenth = -1 + menu.Length;
 		Console.WriteLine(" Note:  Press Key  Number Pad  0 - "+menulenth+"  Button to Change Menu ");
-		Console.WriteLine(MarginSpace(60,0,"=",""));
-		Console.WriteLine(MarginSpace());
-		//Console.WriteLine(MarginSpace(6,appTitle.Length,"=",appTitle));
-		//Console.WriteLine("|      \t\t "+appTitle+" \t\t\t  \t |");
-		Console.WriteLine(MarginSpace(60,appTitle.Length,"=",appTitle));
-
+		tb.field(appTitle);
 		int n=0;
 	}
-//Overriding Example on a method
-	public static String MarginSpace(){
+}
+public class Layout{
+	//Overriding Example on a method
+	public 	int x=60;
+	public String DefaultBodySpace=" ";
+	public Layout(){
+		//constructor;
+	}
+	public Layout(int v){
+		x=v;
+	}
+	public void Width(int v){
+		x=v;
+	}
+	public void setBodyElement(String x){
+		DefaultBodySpace=x;
+	}
+	public  String MarginSpace(){
 		String space="| ";
-		  for(int i=0;i<60;i++){
-		  	 space+="\t";
+		  for(int i=0;i<x;i++){
+		  	 space+=DefaultBodySpace;
 		  }
-		 space+="|";
+		 space+=" |";
 		return space;
 	}
-		public static String MarginSpace(int x,int y,String margin,String bdy){
+		public  String MarginSpace(String margin,String bdy){
+			//int x=getLayoutSize();
+		
+			int y=bdy.Length;
 		String space="| ";
 			int z=0;
 		  for(int i=0;i<x;i++){
-			  
-			 // Console.WriteLine( i >= ((x-y)/2)  && i < ( (  (x-y) /2)+y)  );
 			  if( i >= ((x-y)/2)  && i < ( (  (x-y) /2)+y)  ){
 				  space+=bdy[z];
 				   z++;
-				  
 			  }else{
 				 space+=margin;				 			  
 			  }
-			  
 		  }
 		 space+=" |";
 		return space;
 	}
 }
+public class Table:Layout{
+	public Table(){
+	//consructor
+	}	
+	public void field(String body){
+		Console.WriteLine(MarginSpace("=",""));
+		Console.WriteLine(MarginSpace());
+		Console.WriteLine(MarginSpace(" ",body));
+		Console.WriteLine(MarginSpace());
+		Console.WriteLine(MarginSpace("=",""));
+	}
+}
+	
+	
+	
